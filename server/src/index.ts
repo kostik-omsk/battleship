074ec0e7d38afa64b70f28e639071e079573104b
Kostia S <kosiak5587@gmail.com>
2024-10-23 19:32:33 +0600

    chore: initialize project structure

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello, world!");
+import { WebSocketServer } from "ws";
+
+const PORT = 3000;
+const wss = new WebSocketServer({ port: PORT });
+
+wss.on("connection", (ws) => {
+  console.log("New client connected");
+  console.log(`- Max clients: ${wss.clients.size}`);
+
+  ws.on("message", (message) => {
+    console.log("received: ", message);
+  });
+
+  ws.on("error", (error) => {
+    console.log("Error: %s", error);
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+console.log(`WebSocket server is running on ws://localhost:${PORT}`);
+console.log(`Server parameters:`);
+console.log(`- Port: ${PORT}`);
