7dff337cdf154bf9b9e2162e69ab7be50c6915ec
Kostia S <kosiak5587@gmail.com>
2024-10-25 13:54:10 +0600

    refractor: ParseJSON

--- server/src/utils/messageParser.ts
+++ server/src/utils/messageParser.ts
@@ -1,12 +1,21 @@
-import { GameMessage } from "@/types";
+// import { GameMessage } from "@/types";
 
-export function parseMessage(message: string): GameMessage {
-  const { type, data, id } = JSON.parse(message);
-  const dataToParse = typeof data === "string" && data.length > 0 ? JSON.parse(data) : data;
+// export function parseMessage(message: string) {
+//   const { type, data, id } = JSON.parse(message);
+//   const dataToParse = typeof data === "string" && data.length > 0 ? JSON.parse(data) : data;
 
-  return {
-    type,
-    data: dataToParse,
-    id: id || 0,
-  };
+//   return {
+//     type,
+//     data: dataToParse,
+//     id: id || 0,
+//   };
+// }
+
+export function safeParseJSON<T>(msg: string): T | null {
+  try {
+    return JSON.parse(msg);
+  } catch (error) {
+    console.error("Invalid JSON: ", error);
+    return null;
+  }
 }
